23807b384ca62b3a517345254f84d9d4cdd6089f
bgsDev <bgs07dev@gmail.com>
2024-02-15 15:51:17 +0700

    wes repot

--- resources/views/pdf/suratTugasDaerah.blade.php
+++ resources/views/pdf/suratTugasDaerah.blade.php
@@ -329,12 +329,19 @@
 
                             @foreach ($member as $dt)
                                 <ol style="list-style: disc;">
-                                    <Table>
+                                    <!-- <Table>
                                         <tr><td>{{$loop->index+1}}. </td><td>Nama</td><td>: {{$dt->nmAnggota}}</td></tr>
                                         <tr><td></td><td>NIP</td><td>: {{$dt->nip}}</td></tr>
                                         <tr><td></td><td>Jabatan</td><td>: {{(strlen($dt->nmJabatan)>15 ? $dt->asJabatan:$dt->nmJabatan)}} {{$asdiskab}}</td></tr>
                                         <tr><td></td><td>Golongan</td><td>: {{$dt->golongan}}</td></tr>
                                         <tr><td></td><td>Tingkat</td><td>: {{($dt->tingkat)}}</td></tr>
+                                    </Table> -->
+                                    <Table>
+                                        <tr><td >{{$loop->index+1}}.   </td><td class="verTop">Nama</td><td>:</td><td>{{$dt->nmAnggota}}</td></tr>
+                                        <tr><td></td><td class="verTop">NIP</td><td>:</td><td> {{$dt->nip}}</td></tr>
+                                        <tr><td></td><td class="verTop">Jabatan</td><td class="verTop">:</td><td> {{(strlen($dt->nmJabatan)>15 ? $dt->asJabatan:$dt->nmJabatan)}} {{ $asdiskab}}</td></tr>
+                                        <tr><td></td><td class="verTop">Golongan</td><td>:</td><td> {{$dt->golongan}}</td></tr>
+                                        <tr><td></td><td class="verTop">Tingkat</td><td>:</td><td> {{($dt->tingkat)}}</td></tr>
                                     </Table>
                                 </ol>
                                 @if(count($member)>4)
