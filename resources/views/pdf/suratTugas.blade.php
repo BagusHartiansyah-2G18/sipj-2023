17bdce8eecd3f7f0cb28c8c8b2defe385e63c654
Bagus H <bagushartiansyah07@gmail.com>
2024-07-29 15:52:52 +0700

    lounching APPS

--- resources/views/pdf/suratTugas.blade.php
+++ resources/views/pdf/suratTugas.blade.php
@@ -129,7 +129,7 @@
                     <Table>
                         <tr><td style='min-width: 20px;'>".(count($data)>1 ? ($loop+1).'.':'')."</td><td class='verTop'>Nama</td><td>:</td><td>".$dt['nmAnggota']."</td></tr>
                         <tr><td></td><td class='verTop'>Pangkat/Gol</td><td class='verTop'>:</td><td >".$dt['golongan']."</td></tr>
-                        <tr><td></td><td class='verTop'>NIP</td><td>:</td><td>".$dt['nip']."</td></tr>
+                        <tr><td></td><td class='verTop'>".$dt['snip']."</td><td>:</td><td>".$dt['nip']."</td></tr>
                         <tr><td></td><td class='verTop'>Jabatan</td><td class='verTop'>:</td class='verTop'><td>".$dt['nmJabatan']." "."</td></tr>
                     </Table>
                 ";
