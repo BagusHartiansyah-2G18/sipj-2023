d78fc7c97043e0c05ea8a3c4bd01f92e7d70b34b
Bagus H <bagushartiansyah07@gmail.com>
2024-07-29 14:35:23 +0700

    lounching APPS

--- app/Http/Controllers/PdfGenerator.php
+++ app/Http/Controllers/PdfGenerator.php
@@ -171,7 +171,8 @@ class PdfGenerator extends Controller
         $subPimpinan->alamat= $setda->alamat; 
 
         
-
+        
+        $asKab = 'Kab. Sumbawa Barat';
         
 
         // seleksi data member 
@@ -180,7 +181,7 @@ class PdfGenerator extends Controller
             foreach ($member as $key => $value) {
                 if(empty($value->fileD)){ 
                     $newMember[$key]['nmAnggota']=$value->nmAnggota;
-                    $newMember[$key]['nmJabatan']=$value->nmJabatan;
+                    $newMember[$key]['nmJabatan']=$value->nmJabatan." ".$pimpinan->asDinas." ".$asKab;
                     $newMember[$key]['golongan']=$value->golongan;
                     $newMember[$key]['nip']=$value->nip;
                     $newMember[$key]['tingkatan']=$value->tingkatan;
@@ -189,7 +190,7 @@ class PdfGenerator extends Controller
                 }else{
                     $fileD = json_decode(base64_decode($value->fileD)); 
                     $newMember[$key]['nmAnggota']=$fileD->nmAnggota;
-                    $newMember[$key]['nmJabatan']=$fileD->nmJabatan;
+                    $newMember[$key]['nmJabatan']=$fileD->nmJabatan." ".$pimpinan->asDinas." ".$asKab;
                     $newMember[$key]['golongan']=$fileD->golongan;
                     $newMember[$key]['nip']=$fileD->nip;
                     $newMember[$key]['tingkatan']=$fileD->tingkatan;
