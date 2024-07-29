b755d8043a2527362d04f0c2b076cd7ad375fe9f
Bagus H <bagushartiansyah07@gmail.com>
2024-07-29 13:57:01 +0700

    lounching APPS

--- app/Http/Controllers/PdfGenerator.php
+++ app/Http/Controllers/PdfGenerator.php
@@ -677,7 +677,7 @@ class PdfGenerator extends Controller
         if(!empty($_pim)){
             // $pimpinan =[];
             $pimpinan = json_decode(base64_decode($_pim));  
-            return $this->Mfc->log($pimpinan);
+            // return $this->Mfc->log($_pim);
             
             // $pimpinan->jabatanx= $pimpinan->nmJabatan." ".strtolower(explode(" ",$nmDinas)[0]);
             if($pimpinan->manual == "yes"){
